741d0c90d931409c05d9939dd37d1ec0e25f61f0
Dimario Oliver <dimariooliver@Dimarios-MacBook-Pro.local>
2023-01-20 22:34:06 -0500

    finished

--- index.js
+++ index.js
@@ -1,68 +1,40 @@
 
-function newImage(){
-    let greenCharacter = document.createElement('img')
-    greenCharacter.src = 'assets/green-character.gif'
-    greenCharacter.style.position = 'fixed'
-    greenCharacter.style.left = '100px'
-    greenCharacter.style.bottom = '100px'
-    document.body.append(greenCharacter)
+function newImage(link , left , bottom){
+    let item = document.createElement('img')
+    item.src = link
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom  + 'px'
+    document.body.append(item)
     
-    let pineTree = document.createElement('img')
-    pineTree.src = 'assets/pine-tree.png'
-    pineTree.style.position = 'fixed'
-    pineTree.style.left = '450px'
-    pineTree.style.bottom = '200px'
-    document.body.append(pineTree)
 
-    let tree = document.createElement('img')
-    tree.src = 'assets/tree.png'
-    tree.style.position = 'fixed'
-    tree.style.left = '200px'
-    tree.style.bottom = '300px'
-    document.body.append(tree)
-
-    let pillar = document.createElement('img')
-    pillar.src = 'assets/pillar.png'
-    pillar.style.position = 'fixed'
-    pillar.style.left = '350px'
-    pillar.style.bottom = '100px'
-    document.body.append(pillar)
-
-    let crate = document.createElement('img')
-    crate.src = 'assets/crate.png'
-    crate.style.position = 'fixed'
-    crate.style.left = '150px'
-    crate.style.bottom = '200px'
-    document.body.append(crate)
-
-    let well = document.createElement('img')
-    well.src = 'assets/well.png'
-    well.style.position = 'fixed'
-    well.style.left = '500px'
-    well.style.bottom = '425px'
-    document.body.append(well)   
 }
 
-function newItem(){
-    let sword = document.createElement('img')
-    sword.src = 'assets/sword.png'
-    sword.position = 'fixed'
-    sword.left = '500px'
-    sword.bottom = '405px'
-    document.body.append(sword)
+newImage('assets/green-character.gif', 100 , 100)
+newImage('assets/pine-tree.png', 450 , 200)
+newImage('assets/tree.png', 200 , 300)
+newImage('assets/pillar.png', 350  , 100)
+newImage('assets/crate.png', 150 , 200)
+newImage('assets/well.png', 500 , 425)
 
-    sword.addEventListener('click', function(){
-    sword.remove()
+
+
+
+function newItem(link , left , bottom){
+    let item2= document.createElement('img')
+    item2.src = link
+    item2.position = 'fixed'  
+    item2.left = left + 'px'
+    item2.bottom = bottom + 'px'
+    document.body.append(item2)
+
+    item2.addEventListener('dblclick', function(){
+    item2.remove()
 })
 
+
 }
 
-
-
-
-newImage()
-newItem()
-
-
-
-
+newItem('assets/sword.png',500, 405,)
+newItem('assets/shield.png',165, 185,)
+newItem('assets/staff.png',600, 100)
